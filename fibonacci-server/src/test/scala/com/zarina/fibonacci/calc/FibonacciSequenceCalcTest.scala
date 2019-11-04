3c64cf1bbe5dab81b4b7e0634de60dba118a11ee
Zarina Khadikova <z.khadikova@gmail.com>
2019-11-04 10:12:33 +0100

    Added exception test

--- fibonacci-server/src/test/scala/com/zarina/fibonacci/calc/FibonacciSequenceCalcTest.scala
+++ fibonacci-server/src/test/scala/com/zarina/fibonacci/calc/FibonacciSequenceCalcTest.scala
@@ -1,6 +1,7 @@
 package com.zarina.fibonacci.calc
 
 import org.scalatest._
+
 class FibonacciSequenceCalcTest extends FlatSpec {
 
   "Fibonacci" should "return zero on zero" in {
@@ -11,5 +12,11 @@ class FibonacciSequenceCalcTest extends FlatSpec {
   "Fibonacci" should "calculate sequence correctly" in {
     val number = FibonacciSequenceCalculator.calculateAt(10)
     assert(number === 55)
+  }
+
+  "Fibonacci" should "throw on negative parameter" in {
+    assertThrows[IllegalArgumentException] {
+      FibonacciSequenceCalculator.calculateAt(-1)
+    }
   }
 }
